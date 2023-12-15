8f47d8cc9f920d3bbac6d1fb77f9c9fd2c499b21
Leonardo <leoanrdoisraelcortez@outlook.com>
2023-12-15 00:45:32 -0300

    pseudoterminado home con varias animaciones

--- js/scripts/app.js
+++ js/scripts/app.js
@@ -1,76 +1,47 @@
 // Carrusel
+document.addEventListener("DOMContentLoaded", function () {
+    const carousel = document.getElementById("carousel");
+    const indicatorsContainer = document.getElementById("carousel-indicators");
 
-function setupCarousel() {
-    const container = document.querySelector('.carousel-container');
-    const slides = document.querySelectorAll('.slide');
-    let offset = 0;
-    let slideID = 0;
+    let currentIndex = 0;
 
-    setInterval(() => {
-        offset = slides[0].offsetWidth;
-        container.style.transition = "left ease-in-out 500ms";
-        container.style.left = -offset + 'px';
-        setTimeout(() => {
-            container.style.transition = "none";
-            slides[slideID].style.order = slides.length - 1;
-            container.style.left = 0;
-            slideID++;
-            if (slideID === slides.length) {
-                slideID = 0;
-                slides.forEach(slide => {
-                    slide.style.order = "initial";
-                });
-            }
-        }, 500);
-    }, 3000);
-}
+    function showSlide(index) {
+        carousel.style.transform = `translateX(${-index * 100}%)`;
+        updateIndicators(index);
+    }
+
+    function updateIndicators(index) {
+        const indicators = Array.from(document.querySelectorAll(".indicator"));
+        indicators.forEach((indicator, i) => {
+            indicator.classList.toggle("active", i === index);
+        });
+    }
+
+    function createIndicators() {
+        for (let i = 0; i < carousel.children.length; i++) {
+            const indicator = document.createElement("div");
+            indicator.classList.add("indicator");
+            indicator.addEventListener("click", () => showSlide(i));
+            indicatorsContainer.appendChild(indicator);
+        }
+    }
+
+    function nextSlide() {
+        currentIndex = (currentIndex + 1) % carousel.children.length;
+        showSlide(currentIndex);
+    }
+
+    function startAutoPlay() {
+        setInterval(nextSlide, 3000); // Cambia cada 3 segundos (ajusta según sea necesario)
+    }
+
+    createIndicators();
+    showSlide(currentIndex);
+    startAutoPlay();
+});
 
 // Navbar
-function setupNavbar() {
-    const containernav = document.querySelector('.slider');
-    const slidesnav = document.querySelectorAll('.slidenavelement');
-    let slidenavelementID = 0;
-    let isMouseOver = false;
 
-    function animateSlider() {
-        const offset = slidesnav[0].offsetWidth;
-        containernav.style.transition = "left ease-in-out 1200ms";
-        containernav.style.left = -offset + 'px';
-
-        containernav.addEventListener('transitionend', function handleTransitionEnd() {
-            containernav.style.transition = "none";
-            slidesnav[slidenavelementID].style.order = slidesnav.length - 1;
-            containernav.style.left = 0;
-            slidenavelementID++;
-
-            if (slidenavelementID === slidesnav.length) {
-                slidenavelementID = 0;
-                slidesnav.forEach(slidenavelement => {
-                    slidenavelement.style.order = "initial";
-                });
-            }
-
-            if (!isMouseOver) {
-                requestAnimationFrame(animateSlider);
-            }
-        }, { once: true });
-    }
-
-    function stopAnimation() {
-        isMouseOver = true;
-    }
-
-    function startAnimation() {
-        isMouseOver = false;
-        animateSlider();
-    }
-
-    containernav.addEventListener('mouseenter', stopAnimation);
-    containernav.addEventListener('mouseleave', startAnimation);
-
-    startAnimation();
-}
 
 // Llamadas a las funciones para configurar cada parte
-setupCarousel();
-setupNavbar();
+
